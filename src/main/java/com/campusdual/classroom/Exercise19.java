d0e746707a1972cc329b62eb4591d57c23c51abb
Ana Garces <anagarcestech@gmail.com>
2025-01-16 18:16:12 +0100

    refactorizado ejercicio 19

--- src/main/java/com/campusdual/classroom/Exercise19.java
+++ src/main/java/com/campusdual/classroom/Exercise19.java
@@ -12,18 +12,18 @@ public class Exercise19 {
             sb.append(stringTriMatrixRow(intArrayTri, i));
             sb.append("\n");
         }
-        sb.delete(sb.length()-1, sb.length());
+        sb.delete(sb.length() - 1, sb.length());
         return sb.toString();
 
     }
 
     private static String stringTriMatrixRow(int[][][] intArrayTri, int row) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < intArrayTri.length-1; i++){
+        for (int i = 0; i < intArrayTri.length - 1; i++) {
             sb.append(getUnidimensionalString(intArrayTri[i][row]));
             sb.append(" ");
         }
-        sb.append(getUnidimensionalString(intArrayTri[intArrayTri.length-1][row]));
+        sb.append(getUnidimensionalString(intArrayTri[intArrayTri.length - 1][row]));
 
         return sb.toString();
     }
@@ -33,8 +33,7 @@ public class Exercise19 {
     }
 
     public static String getTridimensionalString(int[][][] intArrayTri) {
-        int[][] flatMatrix = flatTridimensionalArray(intArrayTri);
-        return getTridimensionalString(intArrayTri, flatMatrix);
+        return getTridimensionalString(intArrayTri, flatTridimensionalArray(intArrayTri)); //refactorizado
     }
 
 
@@ -43,10 +42,10 @@ public class Exercise19 {
         int cols = intArrayTri[0][0].length;
         int[][] flatMatrix = new int[rows][cols];
 
-        for (int i = 0; i < intArrayTri.length; i++) { // Para cada capa
-            for (int j = 0; j < rows; j++) {
-                for (int k = 0; k < cols; k++) {
-                    flatMatrix[j][k] += intArrayTri[i][j][k];
+        for (int i = 0; i < intArrayTri.length; i++) { // recorre las capas
+            for (int j = 0; j < rows; j++) { //recorre las filas de cada capa
+                for (int k = 0; k < cols; k++) { //recorre las columnas
+                    flatMatrix[j][k] += intArrayTri[i][j][k]; //suma
                 }
             }
         }
@@ -60,15 +59,16 @@ public class Exercise19 {
             if (i < intArrayBi.length - 1) {
                 sb.append("\n");
             }
-        }   return sb.toString();
+        }
+        return sb.toString();
     }
 
 
     public static String getUnidimensionalString(int[] uniArray) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < uniArray.length; i++){
+        for (int i = 0; i < uniArray.length; i++) {
             sb.append(uniArray[i]);
-            if(i < uniArray.length - 1){
+            if (i < uniArray.length - 1) {
                 sb.append(" ");
             }
         }
@@ -77,18 +77,18 @@ public class Exercise19 {
 
 
     public static int[] createAndPopulateUnidimensionalArray(int columns) {
-            int[] uniArray = new int[columns];
-            for (int i = 0; i < columns; i++) {
-                uniArray[i] = i+1; //el array empieza en 1
-            }
-            return uniArray;
+        int[] uniArray = new int[columns];
+        for (int i = 0; i < columns; i++) {
+            uniArray[i] = i + 1; //el array empieza en 1
+        }
+        return uniArray;
     }
 
     public static int[][] createAndPopulateBidimensionalArray(int rows, int columns) {
         int[][] intArrayBi = new int[rows][columns];
         int valorInicial = 1;
         for (int i = 0; i < rows; i++) {
-            for(int j = 0; j < columns; j++ ){
+            for (int j = 0; j < columns; j++) {
                 intArrayBi[i][j] = valorInicial++;
             }
 
@@ -97,11 +97,11 @@ public class Exercise19 {
     }
 
     public static int[][][] createAndPopulateTridimensionalArray(int depth, int rows, int columns) {
-        int [][][] intArrayTri = new int[depth][rows][columns];
+        int[][][] intArrayTri = new int[depth][rows][columns];
         int valorInicial = 1;
-        for(int i = 0; i < depth; i++){
-            for(int j = 0; j < rows; j++){
-                for(int k = 0; k < columns; k++){
+        for (int i = 0; i < depth; i++) {
+            for (int j = 0; j < rows; j++) {
+                for (int k = 0; k < columns; k++) {
                     intArrayTri[i][j][k] = valorInicial++;
                 }
             }
